4c0ae2706b3656c09b0681ebd4a56c885217694c
aabbell <abelalemneh68@gmail.com>
2025-06-08 13:50:07 +0300

    returned output instead of printing

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,11 @@
 def perform_operation(num1:float ,num2:float ,operation:str):
     if operation == add:
-        return(f"Result: {num1 + num2}")
+        return num1 + num2
     elif operation == substract:
-        return(f"Result: {num1 - num2}")
+        return num1 - num2
     elif operation == multiply:
-        return(f"Result: {num1 * num2}")
+        return num1 * num2
     elif operation == divide:
         if num2 == 0:
             return("num2 can't be 0")
-        return(f"Result: {num1 / num2}")
+        return num1 / num2
