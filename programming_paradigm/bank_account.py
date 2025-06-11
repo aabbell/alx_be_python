3a5f3db6b79d289e4603e92288209259671dfa2e
aabbell <abelalemneh68@gmail.com>
2025-06-11 10:13:35 +0300

    fixed the wthdraw output

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,11 +3,12 @@ class BankAccount:
         self.account_balance = account_balance
     def deposit(self ,amount):
         self.account_balance += amount
-    def withdraw (self, amount):
-        if self.account_balance >= amount:
-            self.account_balance -= amount
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
         else:
-            print("Insufficient funds")
+            self.account_balance -= amount
+            return True
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance}")
         
