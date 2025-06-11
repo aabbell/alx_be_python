c50399cf229ef5d64feb28160b76d59b2d234dfa
aabbell <abelalemneh68@gmail.com>
2025-06-11 10:16:39 +0300

    fixed the dsplay output

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,5 +10,5 @@ class BankAccount:
             self.account_balance -= amount
             return True
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
         
