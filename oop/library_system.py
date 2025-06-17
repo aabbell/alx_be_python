3bf89778e7a353208869408da357eab3ebfd4c81
aabbell <abelalemneh68@gmail.com>
2025-06-17 15:24:26 +0300

    initial commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,7 +5,8 @@ class Book:
     
     def get_details(self):
         return f"Book: {self.title} by {self.author}"
-
+    def __str__(self):
+        return self.get_details()
     
 class EBook(Book):
     def __init__(self, title, author, file_size):
@@ -15,6 +16,8 @@ class EBook(Book):
     def get_details(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size_kb}KB"
 
+    def __str__(self):
+        return self.get_details()
 
 class PrintBook(Book):
     def __init__(self,title,author,page_count):
@@ -22,6 +25,9 @@ class PrintBook(Book):
     
     def get_details(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+    
+    def __str__(self):
+        return self.get_details()
 
 class Library:
     def __init__(self):
